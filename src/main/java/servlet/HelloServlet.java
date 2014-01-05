fca916181c054a8bb6dea383518a9bef64fc137f
iamon3.free@gmail.com <iamon3.free@gmail.com>
2014-01-05 18:10:11 +0530

    Added the support for HTTP client using Apache's Http Client component library.

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import util.ImpressionMarkHttpClient;
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
@@ -19,9 +21,35 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("Page under construction.".getBytes());
+        out.write("Page is under construction. \n".getBytes()/**ImpressionMarkHttpClient.fetchDocument().getBytes()*/);
         out.flush();
         out.close();
     }
-    
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("Page is under construction. \n"/**ImpressionMarkHttpClient.createDocument().getBytes()*/);
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(/**"Page is under construction. \n".getBytes()/**ImpressionMarkHttpClient.updateDocument().getBytes()*/);
+        out.flush();
+        out.close();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("Page is under construction. \n".getBytes()/**ImpressionMarkHttpClient.deleteDocument().getBytes()*/);
+        out.flush();
+        out.close();
+    }
 }
